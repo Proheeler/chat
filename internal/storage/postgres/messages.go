27584d677cf6a41d6efc09cf75bdae073cc60cfe
Andrei Kobzar <drknow12@gmail.com>
2023-02-13 17:25:51 +0400

    Fix history pagination

--- internal/storage/postgres/messages.go
+++ internal/storage/postgres/messages.go
@@ -11,16 +11,6 @@ func (s *PostgresStorage) StoreMessage(msg types.Message, room string) {
 
 func (s *PostgresStorage) ListMessages(room string, offset, limit int) *types.MessageHistory {
 	msgs := []types.Message{}
-	count := int64(0)
-	s.db.Model(&types.Message{}).Where("room = ?", room).Count(&count)
-	if count < int64(limit) {
-		limit = int(count)
-		offset = 0
-	}
-	if count < int64(offset*limit+limit) {
-		offset = int(count) / limit
-		limit = int(count) % limit
-	}
 	s.db.Order("id desc").Limit(limit).Offset(offset).Where("room = ?", room).Find(&msgs)
 	return &types.MessageHistory{
 		Total: len(msgs),
